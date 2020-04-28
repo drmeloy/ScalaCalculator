2a9fd04f10db4fb9c80b8b6a42e032956ecb5bf2
drmeloy <drmeloy@gmail.com>
2020-04-28 11:14:02 -0700

    Creating sealed trait Expression and case classes Addition, Subtraction, and Number

---
+++ src/main/scala/Expression.scala
@@ -0,0 +1,5 @@
+sealed trait Expression
+
+final case class Number(value: Double) extends Expression
+final case class Addition(left: Expression, right: Expression) extends Expression
+final case class Subtraction(left: Expression, right: Expression) extends Expression
