1a5f043b8f56ebc8b1bcaa54b69b9ddde1eafaca
drmeloy <drmeloy@gmail.com>
2020-04-28 11:33:24 -0700

    Redid eval method on calculator

--- src/main/scala/Expression.scala
+++ src/main/scala/Expression.scala
@@ -1,9 +1,43 @@
 sealed trait Expression {
-  def eval: Double =
+  def eval: Calculation =
     this match {
-      case Addition(l, r) => l.eval + r.eval
-      case Subtraction(l, r) => l.eval - r.eval
-      case Number(v) => v
+      case Addition(l, r) =>
+        l.eval match {
+          case Failure(reason) => Failure(reason)
+          case Success(r1) =>
+            r.eval match {
+              case Failure(reason) => Failure(reason)
+              case Success(r2) => Success(r1 + r2)
+            }
+        }
+      case Subtraction(l, r) =>
+        l.eval match {
+          case Failure(reason) => Failure(reason)
+          case Success(r1) =>
+            r.eval match {
+              case Failure(reason) => Failure(reason)
+              case Success(r2) => Success(r1 - r2)
+            }
+        }
+      case Division(l, r) =>
+        l.eval match {
+          case Failure(reason) => Failure(reason)
+          case Success(r1) =>
+            r.eval match {
+              case Failure(reason) => Failure(reason)
+              case Success(r2) =>
+                if (r2 == 0) Failure("Divide by zero")
+                else Success(r1 / r2)
+            }
+        }
+      case SquareRoot(num) =>
+        num.eval match {
+          case Success(r) =>
+            if(r < 0) Failure("Square root of negative number")
+            else Success(Math.sqrt(r))
+          case Failure(reason) => Failure(reason)
+        }
+      case Number(v) => Success(v)
     }
 }
 
