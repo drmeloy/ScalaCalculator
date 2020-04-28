55e5a6ed6b319674bfd6608b7f31596adb39e9ba
drmeloy <drmeloy@gmail.com>
2020-04-28 11:19:40 -0700

    Adding division and square root case classes

--- src/main/scala/Expression.scala
+++ src/main/scala/Expression.scala
@@ -10,3 +10,5 @@ sealed trait Expression {
 final case class Number(value: Double) extends Expression
 final case class Addition(left: Expression, right: Expression) extends Expression
 final case class Subtraction(left: Expression, right: Expression) extends Expression
+final case class Division(left: Expression, right: Expression) extends Expression
+final case class SquareRoot(num: Expression) extends Expression
