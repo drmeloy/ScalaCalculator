32ef40f2e29b498039c833a15fb3b157d0777536
drmeloy <drmeloy@gmail.com>
2020-04-28 11:17:59 -0700

    Adding eval method on Expression trait

--- src/main/scala/Expression.scala
+++ src/main/scala/Expression.scala
@@ -1,4 +1,11 @@
-sealed trait Expression
+sealed trait Expression {
+  def eval: Double =
+    this match {
+      case Addition(l, r) => l.eval + r.eval
+      case Subtraction(l, r) => l.eval - r.eval
+      case Number(v) => v
+    }
+}
 
 final case class Number(value: Double) extends Expression
 final case class Addition(left: Expression, right: Expression) extends Expression
