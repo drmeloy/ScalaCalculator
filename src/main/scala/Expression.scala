cc8db350b4a70747f996b897e02272fa7a403939
drmeloy <drmeloy@gmail.com>
2020-04-28 11:22:33 -0700

    Adding calculation (failure or success) trait

--- src/main/scala/Expression.scala
+++ src/main/scala/Expression.scala
@@ -12,3 +12,8 @@ final case class Addition(left: Expression, right: Expression) extends Expressio
 final case class Subtraction(left: Expression, right: Expression) extends Expression
 final case class Division(left: Expression, right: Expression) extends Expression
 final case class SquareRoot(num: Expression) extends Expression
+
+sealed trait Calculation
+
+final case class Success(result: Double) extends Calculation
+final case class Failure(reason: String) extends Calculation
